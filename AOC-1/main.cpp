5556ee1d6b488b80b1e4947a384f282ecdea989c
Martin Novotný Mlinárcsik <novotny.martin25@gmail.com>
2022-12-02 11:33:39 +0100

    removed sorted

--- AOC-1/main.cpp
+++ AOC-1/main.cpp
@@ -19,52 +19,11 @@
 // The only difference is that now, we store 3 current largest sums (largest at the lowest index) and these values
 // are then compared with each individual successive sum.
 
-// Supports sorted and unsorted modes, sorted used std::sort. This has not shown any performance upgrades.
-// Second parameter: "sorted" for sorted mode, "unsorted" or empty for unsorted mode.
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
-
-void printHelp()
-{
-    std::cout << "---- HELP ----" << std::endl;
-    std::cout << "./fileName [sorted/unsorted/*empty*]" << std::endl;
-    std::cout << "WARNING: -h and [sorted/unsorted/*empty*] CANNOT BE COMBINED" << std::endl;
-    std::cout << "---- HELP ----" << std::endl;
-}
-
-std::vector<int> calculateMostCaloriesSorted(std::fstream& inputFile)
-{
-    int currentCaloriesSum = 0;
-    int currentCalories;
-
-    std::string line;
-
-    std::vector<int>top3_MostCalories = {0,0,0};
-    std::vector<int>elfVec;
-
-    while (std::getline(inputFile, line)){
-        if (line.empty()) {
-            elfVec.emplace_back(currentCaloriesSum);
-            currentCaloriesSum = 0;
-            continue;
-        }
-
-        currentCalories = std::stoi(line, nullptr, 10);
-        currentCaloriesSum += currentCalories;
-    }
-
-    std::sort(elfVec.begin(), elfVec.end(), std::greater<int>());
-
-    top3_MostCalories[0] = elfVec[0];
-    top3_MostCalories[1] = elfVec[1];
-    top3_MostCalories[2] = elfVec[2];
-
-    return top3_MostCalories;
-}
-
 
 std::vector<int> calculateMostCalories(std::fstream& inputFile)
 {
@@ -96,41 +55,20 @@ std::vector<int> calculateMostCalories(std::fstream& inputFile)
     return top3_MostCalories;
 }
 
-int main(int argc, char** argv) {
-
-    std::string argv1; if (argc >= 2) { argv1 = argv[1]; }
-    if (argv1 == "-h") {printHelp(); return 1;}
+int main() {
 
     std::fstream inputFile;
     inputFile.open("input.txt");
     if (!inputFile.is_open()) { std::cerr << "File not opened\n"; exit(1); }
 
-    if (argv1 == "unsorted" || argc == 1) {
-        std::cout << "------ UNSORTED ------" << std::endl;
+    std::vector<int> values = calculateMostCalories(inputFile);
+    int sum = 0;
 
-        std::vector<int> values = calculateMostCalories(inputFile);
-        int sum = 0;
-
-        for (int i = 0; i < values.size(); i++) {
-            sum += values[i];
-            std::cout << "Value " << i << ": " << values[i] << std::endl;
-        }
-
-        std::cout << "Total sum: " << sum << std::endl;
+    for (int i = 0; i < values.size(); i++) {
+        sum += values[i];
+        std::cout << "Value " << i << ": " << values[i] << std::endl;
     }
 
-    else if (argv1 == "sorted") {
-        std::cout << "------ SORTED ------" << std::endl;
-
-        std::vector<int> values = calculateMostCaloriesSorted(inputFile);
-        int sum = 0;
-
-        for (int i = 0; i < values.size(); i++) {
-            sum += values[i];
-            std::cout << "Value " << i << ": " << values[i] << std::endl;
-        }
-
-        std::cout << "Total sum: " << sum << std::endl;
-    }
+    std::cout << "Total sum: " << sum << std::endl;
     return 0;
 }
