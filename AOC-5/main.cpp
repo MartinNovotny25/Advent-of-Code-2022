c241f09c4dcc1b5fabde95ce39186ecb21f614b4
MartinNovotny25 <novotny.martin25@gmail.com>
2023-05-30 20:54:47 +0200

    Added comments and frees

--- AOC-5/main.cpp
+++ AOC-5/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdio>
+#include <cstring>
 
 typedef struct instructionParams {
     int moveFrom;
@@ -21,7 +22,6 @@ char* createArray(int columnsNum, int rowsNum, FILE* inputFile);
 int moveBlocks(char* p_array, int* maxColumnHeights, int* currentHeights, instructionParams instruction, int amount, int colsNum);
 int moveCursorToInstructions(FILE* inputFile, int columnsNum, int rowsNum);
 int numberOfInstructions(FILE* inputFile);
-void printArray(char* p_array, int columnsNum, int rowsNum);
 void printArray(char* p_array, int size);
 instructionParams getInstruction(FILE* inputFile);
 int getStackTop(char* p_array, int skipIndexes, int columnSize);
@@ -42,41 +42,59 @@ void detectMissingBlock(char* array, int* maxHeights);
 
 
 int main() {
+
+    // Open the file for reading
     FILE* inputFile;
     inputFile = fopen("AOC-5/input.txt", "r");
-    if (inputFile == nullptr) {
+    if (inputFile == NULL) {
         std::cerr << "File not found" << std::endl;
         fclose(inputFile);
     }
 
-    const int columnsNum = parseColumns(inputFile);
-    const int rowsNum = parseLines(inputFile);
+    const int columnsNum = parseColumns(inputFile); // Retrieve the number of columns
+    const int rowsNum = parseLines(inputFile); // Retrieve the number of rows
 
-    char* array = createArray(columnsNum, rowsNum, inputFile);
+    char* array = createArray(columnsNum, rowsNum, inputFile); // Create array to store crates
+    // Create array to store current heights of columns
     int* columnHeights = calculateInitialHeight(array, rowsNum, columnsNum);
+    // Create array to store maximum heights of columns
     int* maxColumnHeights = createMaxColumnHeights(rowsNum, columnsNum);
 
-    moveCursorToInstructions(inputFile, columnsNum, rowsNum);
+    moveCursorToInstructions(inputFile, columnsNum, rowsNum); // Move cursor to the beginning of instructions
 
-    const int instructionCount = numberOfInstructions(inputFile);
+    const int instructionCount = numberOfInstructions(inputFile); // Get the number of instructions
     moveCursorToInstructions(inputFile, columnsNum, rowsNum);
-    printArray(array, columnsNum, rowsNum);
 
     for (short int i = 0; i < instructionCount; ++i) {;
-        instructionParams instruction = getInstruction(inputFile);
+        instructionParams instruction = getInstruction(inputFile); // Parse the instruction
+
+        // While the current maximum height (memory allocated for the given column) is lower than the current
+        // height + the amount of crates that will be placed there, resize the moveTo column
         while (columnHeights[instruction.moveTo] + instruction.amount > maxColumnHeights[instruction.moveTo]) {
-            array = resizeArray(&array, maxColumnHeights, columnHeights,instruction.moveTo, columnsNum);
+            array = resizeArray(&array, maxColumnHeights, columnHeights,
+                                instruction.moveTo, columnsNum);
         }
 
+        // Perform the move
         moveBlocks(array, maxColumnHeights,columnHeights, instruction, instruction.amount, columnsNum);
+        // Check for errors ion movement
         detectMissingBlock(array, maxColumnHeights);
     }
-    printArray(array, maxColumnHeights[columnsNum]);
+
+    printArray(array, maxColumnHeights[columnsNum]); // Prints the final array
+
     fclose(inputFile);
-    //free(array);
+    free(array);
     free(columnHeights);
     free(maxColumnHeights);
 }
+
+/*
+ * Function returns number of columns
+ * Every read symbol increases the counter
+ * and by dividing the counter by 4,
+ * we get the number of letters 1 row;
+ */
 
 int parseColumns(FILE* inputFile) {
     int symbol;
@@ -90,6 +108,11 @@ int parseColumns(FILE* inputFile) {
     return (n >> 2);
 }
 
+/*
+ * Function counts the number rows by
+ * counting endline characters
+ */
+
 int parseLines(FILE* inputFile) {
     int symbol = '\0';
     int n = 0;
@@ -102,14 +125,24 @@ int parseLines(FILE* inputFile) {
     return n;
 }
 
+/*
+ * Function parses 1 instruction line and returns a structure with
+ * its parameters.
+ */
+
 instructionParams getInstruction(FILE* inputFile) {
     instructionParams instruction;
 
     int symbol1, symbol2;
     int val = 0;
+
+    // Skips word "move" by advancing the file cursor by 5 characters
     fseek(inputFile, 5, SEEK_CUR);
     symbol1 = fgetc(inputFile);
     symbol2 = fgetc(inputFile);
+
+    // In case the number is double-digit, the first number has to be multiplied by 10 and added to the second number
+    // As they are read as single digits, and the cursor will be moved back one character
     if (symbol2 == ' ') { val = symbol1 - ASCII_CONSTANT; fseek(inputFile, -1, SEEK_CUR);}
     else {
         val = 10*(symbol1 - ASCII_CONSTANT) + (symbol2 - ASCII_CONSTANT);
@@ -117,6 +150,7 @@ instructionParams getInstruction(FILE* inputFile) {
 
     instruction.amount = val;
 
+    // Skips word "from" by advancing the file cursor by 6 characters, includes whitespaces
     fseek(inputFile, 6, SEEK_CUR);
     symbol1 = fgetc(inputFile);
     symbol2 = fgetc(inputFile);
@@ -127,6 +161,7 @@ instructionParams getInstruction(FILE* inputFile) {
 
     instruction.moveFrom = val -1;
 
+    // Skips word "to" by advancing the file cursor by 4 characters, includes whitespaces
     fseek(inputFile, 4, SEEK_CUR);
     symbol1 = fgetc(inputFile);
     symbol2 = fgetc(inputFile);
@@ -141,12 +176,18 @@ instructionParams getInstruction(FILE* inputFile) {
     return instruction;
 }
 
+/*
+ * Function moves cursor the to beginning of instructions
+ */
 int moveCursorToInstructions(FILE* inputFile, int columnsNum, int rowsNum) {
     int offset = ((columnsNum * (BRACKETS + COLUMN_SPACE + LETTER)) * (rowsNum + 1)) + END_LINE;
     fseek(inputFile, offset, SEEK_SET);
     return 0;
 }
 
+/*
+ * Function counts the number of instructions
+ */
 int numberOfInstructions(FILE* inputFile) {
     int symbol = '\0';
     int count = 0;
@@ -158,29 +199,44 @@ int numberOfInstructions(FILE* inputFile) {
     fseek(inputFile, 0, SEEK_SET);
     return count;
 }
-
+/*
+ * Function creates the initial array of size columns * rows. After memory allocation,
+ * all indexes are set to NULL. Function then goes through the crates in row order
+ * and stores the letters into the array IN COLUMN ORDER, so that
+ * columns are stored next to each other.
+ */
 char* createArray(int columnsNum, int rowsNum, FILE* inputFile) {
-   char* array = (char*) malloc(sizeof(char) * columnsNum * rowsNum);
-   memset(array, '\0' ,columnsNum * rowsNum);
-   char symbol = '\0';
-   const int num_SymbolsInLine = columnsNum * (LETTER + BRACKETS + COLUMN_SPACE);
-   const int letterOffset = BRACKETS + COLUMN_SPACE;
+    char* array = (char*) malloc(sizeof(char) * columnsNum * rowsNum);
+    memset(array, '\0' ,columnsNum * rowsNum);
+    char symbol = '\0';
+    const int num_SymbolsInLine = columnsNum * (LETTER + BRACKETS + COLUMN_SPACE);
+    const int letterOffset = BRACKETS + COLUMN_SPACE;
 
-   // Fill the array
-   for (unsigned int i = 0; i < rowsNum; ++i) {
-       fseek(inputFile, i * num_SymbolsInLine, SEEK_SET);
-       for (unsigned int j = 0; j < columnsNum; ++j) {
-           (j == 0) ? fseek(inputFile, 1, SEEK_CUR) : fseek(inputFile, letterOffset, SEEK_CUR);
-           symbol = fgetc(inputFile);
-           symbol == ' ' ? array[j * rowsNum + i] = '\0' : array[j * rowsNum + i] = char(symbol);
-       }
-   }
-   return array;
+    // Fill the array
+    for (unsigned int i = 0; i < rowsNum; ++i) {
+        fseek(inputFile, i * num_SymbolsInLine, SEEK_SET);
+        for (unsigned int j = 0; j < columnsNum; ++j) {
+            (j == 0) ? fseek(inputFile, 1, SEEK_CUR) : fseek(inputFile, letterOffset, SEEK_CUR);
+            symbol = fgetc(inputFile);
+            symbol == ' ' ? array[j * rowsNum + i] = '\0' : array[j * rowsNum + i] = char(symbol);
+        }
+    }
+    return array;
 }
 // Array operation logic
 // fromOffset - Index of the block that is being moved
 // toOffset - Index of the empty space, to which the block is being moved to
+
+/*
+ * Function moves crates between columns.
+ * fromOffset is the index of the first block that is being moved
+ * toOffset is the index of an empty space, to which a block is being moved
+ * moveToSkip and moveFromSkip are values that tell the function how many indexes they have to skip
+ * to get to the column that is being altered. These values are used in getStackTop, which will use them to
+ * get to the wanted columns and then add the number of NULL values (empty spaces), until it reaches first crate.
+ */
 int moveBlocks(char* p_array, int* maxColumnHeights, int* currentHeights, instructionParams instruction, int amount, int colsNum) {
+
     int moveToSkip = 0, moveFromSkip = 0;
     for (int i = 0; i < instruction.moveTo; i++) {
         moveToSkip += maxColumnHeights[i];
@@ -194,16 +250,21 @@ int moveBlocks(char* p_array, int* maxColumnHeights, int* currentHeights, instru
     if (toOffset > 0) {toOffset--;}
     toOffset -= amount -1;
 
+    // Crates will retain their original order
     for (int i = 0; i < amount; ++i) {
        p_array[toOffset + i] = p_array[fromOffset + i];
        p_array[fromOffset + i] = '\0';
     }
 
+    // Update the current sizes of both columns
     currentHeights[instruction.moveTo] += amount;
     currentHeights[instruction.moveFrom] -= amount;
     return 0;
 }
 
+/*
+ * Function returns a value, that gives the current index, on which the current column begins.
+ */
 int getStackTop(char* p_array, int skipIndexes, int columnSize) {
     char symbol = '\0';
     for (int i = 0; i < columnSize; i++) {
@@ -214,12 +275,20 @@ int getStackTop(char* p_array, int skipIndexes, int columnSize) {
     return skipIndexes + columnSize;
 }
 
+/*
+ * Function resizes the array where the crates are stored by a MEMORY_BLOCK value.
+ * Afterwards, it copies the values precede the expanded column.
+ * Crates in the expanded column are placed in the back of the column, moved back by
+ * MEMORY_BLOCK places. The rest of the array is copied after.
+ */
+
 char* resizeArray(char** p_oldArray, int* maxColumnHeights, int* columnHeights, int column, int columnNum) {
     // Create new, expanded array
     char* p_newArray = (char *) malloc( (maxColumnHeights[columnNum] + MEMORY_BLOCK));
     memset(p_newArray, '\0', maxColumnHeights[columnNum] + MEMORY_BLOCK);
 
     // Memcpy the columns before the expanded column into the new array (excludes column 0)
+    // Count the number of bytes that have to be copied before the expanded column.
     int numOfBytesPreCols = 0;
     if (column != 0) {
         for (int i = 0; i < column; ++i) {
@@ -258,26 +327,35 @@ char* resizeArray(char** p_oldArray, int* maxColumnHeights, int* columnHeights, 
 //        std::cout << &p_oldArray[i] << std::endl;
 //        free(&p_oldArray[i]);
 //    }
-    //free(*p_oldArray);
+    free(*p_oldArray);
+
+    // Update the current maximum heights of both columns.
     maxColumnHeights[column] += MEMORY_BLOCK;
     maxColumnHeights[columnNum] += MEMORY_BLOCK;
+
     return p_newArray;
 }
 
+/*
+ * Function creates an array of initial heights of each column and returns a pointer.
+ */
 int* calculateInitialHeight(char* p_array, int columnSize, int columnNum) {
     int index = 0;
     int* heights = (int*) malloc(sizeof(int) * (columnNum + SUM_OF_HEIGHTS_INDEX));
     memset(heights, 0, sizeof(&heights));
-    //for (int i = 0; i < columnNum; ++i) { heights[i] = 0;}
-
     for (int i = 0; i < (columnNum*columnSize); i = i + columnSize) {
         for (int j = 0; j < columnSize; ++j) {
             p_array[i + j] == '\0' ? : (heights[index]++, heights[columnNum]++);
         }
         index++;
     }
+
     return heights;
 }
+
+/*
+ * Function creates an array of maximum heights of each column and returns a pointer.
+ */
 
 int* createMaxColumnHeights(int rowsNum, int columnsNum) {
     int* maxColumnHeights = (int*) malloc(sizeof(int) * (columnsNum + SUM_OF_HEIGHTS_INDEX));
@@ -288,19 +366,19 @@ int* createMaxColumnHeights(int rowsNum, int columnsNum) {
     return maxColumnHeights;
 }
 
-void printArray(char* p_array, int columnsNum, int rowsNum) {
-    for (int i = 0; i < columnsNum * rowsNum; ++i){
-        std::cout << p_array[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
+/*
+ * Function for printing arrays.
+ */
 void printArray(char* p_array, int size) {
     for (int i = 0; i < size; ++i){
         std::cout << p_array[i] << " ";
     }
     std::cout << std::endl;
 }
+
+/*
+ * Debug function, detects faulty crate movement
+ */
 
 void detectMissingBlock(char* array, int* maxHeights) {
     int count = 0;
@@ -317,6 +395,7 @@ void detectMissingBlock(char* array, int* maxHeights) {
         exit(-2);
     }
 }
+
 
 
 
