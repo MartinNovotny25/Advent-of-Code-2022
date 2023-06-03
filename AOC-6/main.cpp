baf5db8989d5a8bd55b42e08f056b5edc9de26d8
MartinNovotny25 <novotny.martin25@gmail.com>
2023-06-03 14:51:22 +0200

    Finished part 2, need to separate part1 and part2

--- AOC-6/main.cpp
+++ AOC-6/main.cpp
@@ -9,29 +9,65 @@
 #include <cstdio>
 #include <cstring>
 
-#define SLIDING_WINDOW 4
+#define SLIDING_WINDOW 14
 
 void initialFill(char* array, FILE* inputFile);
+bool initialComparison(char* array, int* skipComparison);
 void printArray(char* array);
+void shiftArray(char* array, char symbol);
+bool compare(char* array, int* skipComparison, int* compareOffset);
 
 int main() {
-    FILE* inputFile;
+    FILE *inputFile;
     inputFile = fopen("AOC-6/input.txt", "r");
     if (inputFile == nullptr) {
         std::cout << "Input file not found" << std::endl;
     }
 
-    char array[4] = {0};
-    printArray(array);
+    char array[14] = {0};
+    char symbol = '\0';
+    int skipComparison = 0;
+    int counter = 0;
+    int compareOffset = 0;
+
     initialFill(array, inputFile);
     printArray(array);
-
-
-    return 0;
+    counter = 14;
+    if (initialComparison(array, &skipComparison) == true) {
+        printArray(array);
+        std::cout << "Counter value: " << counter << std::endl;
+        return 0;
+    }
+    while (symbol != EOF) {
+        symbol = fgetc(inputFile);
+        counter++;
+        shiftArray(array, symbol);
+        printArray(array);
+        //if (skipComparison == 0) {
+            if (compare(array, &skipComparison, &compareOffset) == true) {
+                printArray(array);
+                std::cout << "Counter value: " << counter << std::endl;
+                exit(0);
+            }
+       // } //else {skipComparison--; compareOffset--;}
+        //    else {skipComparison--;}
+    }
+    std::cout << "NOT FOUND" << std::endl;
+    return 1;
 }
 
-void shiftArray(char* array) {
-
+bool compare(char* array, int* skipComparison, int* compareOffset) {
+    for (int i = 0; i < SLIDING_WINDOW - 1; ++i) {
+        //for (int j = i + 1 + *compareOffset; j < SLIDING_WINDOW; ++j) {
+        for (int j = i + 1; j < SLIDING_WINDOW; ++j) {
+            if (array[i] == array[j]) {
+                //*skipComparison = i + 1;
+                //*compareOffset = i + 1;
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 void initialFill(char* array, FILE* inputFile) {
@@ -40,8 +76,28 @@ void initialFill(char* array, FILE* inputFile) {
     }
 }
 
+bool initialComparison(char* array, int* skipComparison) {
+    for (int i = 0; i < SLIDING_WINDOW - 1; ++i) {
+        for (int j = i + 1; j < SLIDING_WINDOW; ++j) {
+            if (array[i] == array[j]) {
+                //*skipComparison = i + 1;
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void shiftArray(char* array, char symbol) {
+    for (int i = 0; i < SLIDING_WINDOW; ++i) {
+        i != SLIDING_WINDOW - 1 ? array[i] = array[i+1] : array[i] = symbol;
+    }
+}
+
 void printArray(char* array) {
     for(int i = 0; i < SLIDING_WINDOW; ++i) {
-        std::cout << "[" << i << "]: " << array[i] << std::endl;
+        std::cout << "[" << i << "]: " << array[i];
     }
+
+    std::cout << std::endl;
 }
